0dd73cf68c882c73fa5ad9549f7b88edf6f79fb0
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2018-07-19 18:02:00 -0500

    added socket event handlers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 let app = express();
+//need to configure express with http for socket.io
+let server = http.createServer(app);
+//creates a web socket server
+let io = socketIO(server)
 
 //Middleware
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+//register event listener
+//event type is 1st arg
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.on('disconnect', () => {
+		console.log('Client was disconnected');
+	});
+});
+
+server.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
