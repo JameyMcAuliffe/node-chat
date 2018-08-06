d5052193f4b380bed6e76857754b82f0099e905e
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2018-08-06 17:22:49 -0500

    added rooms class, methods for updating rooms list

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const http = require('http');
 const {generateMessage, generateLocation} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
 const {Users} = require('./utils/users');
+const {Rooms} = require('./utils/rooms');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ let server = http.createServer(app);
 //creates a web socket server
 let io = socketIO(server)
 let users = new Users();
+let rooms = new Rooms();
 
 //Middleware
 app.use(express.static(publicPath));
@@ -25,20 +27,33 @@ io.on('connection', (socket) => {
 	console.log('New user connected');
 
 	socket.on('join', (params, callback) => {
-		if (!isRealString(params.name) || !isRealString(params.room)) {
+		let room = params.room.toLowerCase();
+
+		if (!isRealString(params.name) || !isRealString(room)) {
 			return callback('Name and room name are required');
-		}
+		} 
+
+		users.getUserList(room).map((user) => {
+			if(user === params.name) {
+				return callback('Name already exists, please choose another');
+			}
+		});
 
 		//socket.join takes a string
-		socket.join(params.room);
+		socket.join(room);
 		//remove user from any previous rooms before adding to new one
 		users.removeUser(socket.id);
-		users.addUser(socket.id, params.name, params.room);
+		users.addUser(socket.id, params.name, room);
+		
+		if(!rooms.rooms.includes(room)) {
+			rooms.addRoom(room);
+		}
+		console.log('Rooms:', rooms.getRooms());
 
 		//only emits to specific room
-		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+		io.to(room).emit('updateUserList', users.getUserList(room));
 		socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
-		socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `User ${params.name} has joined.`));
+		socket.broadcast.to(room).emit('newMessage', generateMessage('Admin', `User ${params.name} has joined.`));
 
 		callback();
 	});
@@ -64,6 +79,17 @@ io.on('connection', (socket) => {
 
 	socket.on('disconnect', () => {
 		let user = users.removeUser(socket.id);
+		let roomKeys = Object.keys(socket.adapter.rooms);
+		// console.log('socket: ', JSON.stringify(socket.adapter.rooms, undefined, 2));
+		// console.log('Keys:', roomKeys);
+
+
+		rooms.getRooms().map((room) => {
+			if(!roomKeys.includes(room)) {
+				rooms.removeRoom(room);
+				console.log('After remove: ', rooms.getRooms());
+			}
+		});
 
 		if(user) {
 			io.to(user.room).emit('updateUserList', users.getUserList(user.room));
